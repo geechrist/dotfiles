cfd94e4fb0f198f0ffea17efaa15045ffcc14d55
geechrist <geechrist@gmail.com>
2016-01-18 04:11:20 +0800

    add fasd support

---
+++ fasd/init.zsh
@@ -0,0 +1,10 @@
+fasd_cache="$HOME/.fasd-init-zsh"
+if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
+  fasd --init posix-alias zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install >| "$fasd_cache"
+fi
+source "$fasd_cache"
+unset fasd_cache
+
+alias j='fasd_cd -i'
+alias d='fasd_cd -d'
+alias f="fasd -f -e vim"
