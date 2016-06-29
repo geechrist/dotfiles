12bf1e3a10f12cc8a0fbce9a17c778617d840694
Zhen She <geechrist@gmail.com>
2016-06-29 18:09:56 +0800

    lazy load nvm

--- node/path.zsh
+++ node/path.zsh
@@ -2,4 +2,9 @@
 export PATH="$PATH:/usr/local/share/npm/bin:$HOME/.npm/bin"
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+nvm() {
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+  nvm $@
+}
