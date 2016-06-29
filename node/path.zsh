fae1b930c6c562b5b235ed0a15005b3314c13a83
Zhen She <geechrist@gmail.com>
2016-06-29 14:39:58 +0800

    more generic PATH expression

--- node/path.zsh
+++ node/path.zsh
@@ -2,5 +2,5 @@
 export PATH="/usr/local/share/npm/bin:$PATH"
 export PATH="$PATH:$HOME/.npm/bin"
 
-export NVM_DIR="/Users/Thoughtworks/.nvm"
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
